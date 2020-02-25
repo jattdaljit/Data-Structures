baabd09f64d935e55ad2abc96ce9685dba49fe67
Daljit_Singh@epam.com <@Sinjit$1212%>
2020-02-25 15:50:46 +0530

    modified find max in array using recursion

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+O
+Add two number linked list.java,2\e\2e0859cde347fccd81ba6b9b72e644a7c743e6c7
+H
+kth largest element.java,8\8\88fb7ef46b27249dddac61270a0727e309458910
+V
+&Find max in array using recursion.java,1\2\121b809b58c07fdd5de6612e140be0e20e349766
--- Find max in array using recursion.java
+++ Find max in array using recursion.java
@@ -2,11 +2,15 @@ public class Test {
 
     public static void main(String[] args) {
         int[] a = {1, 4, 6, 8, 7, 79};
-        System.out.println(findMax(a, 0));
+        System.out.println(findMax(a));
 
     }
 
-    static public int findMax(int[] a, int i) {
+    public static int findMax(int[] a) {
+        return findMax(a, 0);
+    }
+
+    static private int findMax(int[] a, int i) {
         if (i == a.length - 1) return a[i];
         return Math.max(a[i], findMax(a, i + 1));
     }
